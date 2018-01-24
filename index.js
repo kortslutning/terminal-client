61a43e1267262bef3e41162874189fb0d9553a88
Mikael Brevik <mikaelbre@gmail.com>
2018-01-24 10:35:12 +0100

    Simplifies code, using api instead of rss feed

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 #!/usr/bin/env node
 
-var FeedParser = require('feedparser');
-var request = require('request');
-var Speaker = require('speaker');
-var lame = require('lame');
-var inquirer = require('inquirer');
+const podcast = 4239;
+const inquirer = require('inquirer');
+const Speaker = require('speaker');
+const request = require('request');
+const lame = require('lame');
+const bent = require('bent');
+const ora = require('ora');
 
-var req = request('https://rss.simplecast.com/podcasts/4239/rss');
-var feedparser = new FeedParser();
+const getJson = bent('json');
+
+function getEpisodes() {
+  return getJson(
+    `https://kortslutning-server-qtoearljfv.now.sh/v1/podcasts/${podcast}/episodes.json`
+  );
+}
 
 var options = {
   channels: 2,
@@ -15,54 +22,25 @@ var options = {
   sampleRate: 44100
 };
 
-var decoder = new lame.Decoder(options);
-var speaker = new Speaker(options);
+const decoder = new lame.Decoder(options);
+const speaker = new Speaker(options);
 
-req.on('error', function(error) {
-  console.error(error);
-  process.exit(1);
-});
-
-req.on('response', function(res) {
-  var stream = this; // `this` is `req`, which is a stream
-
-  if (res.statusCode !== 200) {
-    this.emit('error', new Error('Bad status code'));
-  } else {
-    stream.pipe(feedparser);
-  }
-});
-
-feedparser.on('error', function(error) {
-  console.error(error);
-  process.exit(1);
-});
+getEpisodes().then(selectSong);
 
 function getMp3(item) {
-  return request(item.enclosures[0].url);
+  return request(item.audio_url);
 }
 
 function startPlaying(item) {
+  ora({
+    text: `Playing ${item.title}`,
+    spinner: 'simpleDotsScrolling'
+  }).start();
+
   getMp3(item)
     .pipe(decoder)
     .pipe(speaker);
 }
-
-const items = [];
-feedparser
-  .on('readable', function() {
-    // This is where the action is!
-    var stream = this; // `this` is `feedparser`, which is a stream
-    var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
-    var item;
-
-    while ((item = stream.read())) {
-      items.push(item);
-    }
-  })
-  .on('finish', function() {
-    selectSong(items);
-  });
 
 function selectSong(items) {
   inquirer
